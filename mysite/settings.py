959aec7bd8067bd3dd543fe8d92f168d1fce92a6
ffischer <franziskafischer93@gmail.com>
2017-10-14 15:53:01 +0200

    upadated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7$pru2d#+2d#j$-^r230jgwg-4orb_&r(2p+#*@^s_^ywwuo4d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<Dell>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'waterfall08.pythonanywhere.com']
 
 
 # Application definition
